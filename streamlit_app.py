d3006dcf42499c74782868a1cfadcbe3a66dcd1e
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-06-01 15:39:48 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,52 +20,53 @@ def read_dados():
     return dados_function
 
 dados = read_dados()
-crs = st.selectbox('COORDENADORIA REGIONAL DE SAÚDE', options=sorted(dados['Regional de Saúde'].unique()), index=None, placeholder='Selecione uma CRS', key='crs')
+col1, col2 = st.columns([1,2])
+with col1:
+    crs = st.selectbox('COORDENADORIA REGIONAL DE SAÚDE', options=sorted(dados['Regional de Saúde'].unique()), index=None, placeholder='Selecione uma CRS', key='crs')
         
-# Cria um seletor para escolher o município com base na Regional de Saúde selecionada
-#municipio = st.selectbox('MUNICÍPIO', options=sorted(dados[dados['Regional de Saúde']==crs]['Município'].unique()), index=None, placeholder='Selecione uma município', key='municipio')
-       
-# Criação do DataFrame
-if crs != None:
-    df = dados[dados['Regional de Saúde']==crs].reset_index(drop=True)
+    # Cria um seletor para escolher o município com base na Regional de Saúde selecionada
+    #municipio = st.selectbox('MUNICÍPIO', options=sorted(dados[dados['Regional de Saúde']==crs]['Município'].unique()), index=None, placeholder='Selecione uma município', key='municipio')
+           
+    # Criação do DataFrame
+    if crs != None:
+        df = dados[dados['Regional de Saúde']==crs].reset_index(drop=True)
+    
+    else:
+        df = dados.copy()
 
-else:
-    df = dados.copy()
-
-#pontos_captacao_rs_2024_com_lat_lon
-
-# URL do arquivo GeoJSON
-url = 'https://github.com/andrejarenkow/geodata/raw/main/municipios_rs_CRS/RS_Municipios_2021.json'
-
-# Carregar o arquivo GeoJSON via URL
-response = requests.get(url)
-geojson_data = response.json()
-
-
-# Configurar o token do Mapbox
-token = 'pk.eyJ1IjoiYW5kcmUtamFyZW5rb3ciLCJhIjoiY2xkdzZ2eDdxMDRmMzN1bnV6MnlpNnNweSJ9.4_9fi6bcTxgy5mGaTmE4Pw'
-px.set_mapbox_access_token(token)
-
-
-
-# Criação do mapa com os pontos
-fig = px.scatter_mapbox(
-    df, 
-    lat='Latitude_corrigida', 
-    lon='Longitude_corrigida', 
-    height=800,
-    hover_name='Nome da Forma de Abastecimento', 
-    hover_data=['Município', 'Nome da Instiuição'], 
-    #color='Tipo da Forma de Abastecimento'
-)
-
-
-
-# Configuração do mapa
-fig.update_layout(
-    mapbox_style="dark",
-    mapbox_zoom=6,
-    mapbox_center={"lat": -29.5, "lon": -53.5}
-)
-
-st.plotly_chart(fig)
+with col2:
+    # URL do arquivo GeoJSON
+    url = 'https://github.com/andrejarenkow/geodata/raw/main/municipios_rs_CRS/RS_Municipios_2021.json'
+    
+    # Carregar o arquivo GeoJSON via URL
+    response = requests.get(url)
+    geojson_data = response.json()
+    
+    
+    # Configurar o token do Mapbox
+    token = 'pk.eyJ1IjoiYW5kcmUtamFyZW5rb3ciLCJhIjoiY2xkdzZ2eDdxMDRmMzN1bnV6MnlpNnNweSJ9.4_9fi6bcTxgy5mGaTmE4Pw'
+    px.set_mapbox_access_token(token)
+    
+    
+    
+    # Criação do mapa com os pontos
+    fig = px.scatter_mapbox(
+        df, 
+        lat='Latitude_corrigida', 
+        lon='Longitude_corrigida', 
+        height=800,
+        hover_name='Nome da Forma de Abastecimento', 
+        hover_data=['Município', 'Nome da Instiuição'], 
+        #color='Tipo da Forma de Abastecimento'
+    )
+    
+    
+    
+    # Configuração do mapa
+    fig.update_layout(
+        mapbox_style="dark",
+        mapbox_zoom=6,
+        mapbox_center={"lat": -29.5, "lon": -53.5}
+    )
+    
+    st.plotly_chart(fig)
