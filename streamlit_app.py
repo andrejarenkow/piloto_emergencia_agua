56bf2f1fa74668b1f116c8da8ab55662a15407b9
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-06-14 17:45:53 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,9 +19,9 @@ st.set_page_config(
 
 col1, col2, col3 = st.columns([1,4,1])
 
-col1.image('https://github.com/andrejarenkow/csv/blob/master/logo_cevs%20(2).png?raw=true', width=100)
+col3.image('https://github.com/andrejarenkow/csv/blob/master/logo_cevs%20(2).png?raw=true', width=100)
 col2.subheader('Formas de abastecimento de água geolocalizadas e área inundada RS, maio 2024')
-col3.image('https://github.com/andrejarenkow/csv/blob/master/logo_estado%20(3)%20(1).png?raw=true', width=150)
+col1.image('https://github.com/andrejarenkow/csv/blob/master/logo_estado%20(3)%20(1).png?raw=true', width=150)
 
 # Lê os dados de um arquivo Excel online
 @st.cache_data
