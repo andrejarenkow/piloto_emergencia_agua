640a5a70fa2408f1541cb0b47d12e12132f381ee
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-07-01 10:56:42 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -178,7 +178,7 @@ Em seguida, foi gerado um buffer de 500 metros ao redor de todos os pontos de ab
 tratando-os como pontos de alerta para futuros eventos climáticos extremos.
             """
     )
-    gdf_pontos
+    gdf_pontos = pd.DataFrame(gdf_pontos)
     # Passo 2: Converter o GeoDataFrame em GeoJSON
     geojson = gdf_area_inundada.to_crs(epsg=4326).__geo_interface__
     # Passo 3: Criar o mapa scatter_mapbox usando Plotly Express
@@ -190,8 +190,8 @@ tratando-os como pontos de alerta para futuros eventos climáticos extremos.
     
     fig = px.scatter_mapbox(
         gdf_pontos,
-        lat=gdf_pontos.geometry.centroid().x,
-        lon=gdf_pontos.geometry.centroid().y,
+        lat='Latitude_corrigida,
+        lon='Latitude_corrigida',
         #hover_name='name',
         zoom=5,
         height=800
