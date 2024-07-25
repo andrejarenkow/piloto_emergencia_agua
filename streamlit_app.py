8d671c775879bbbe892197cf90d1dbade3173724
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-07-25 17:35:01 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -371,7 +371,7 @@ with tab_resultados:
 
         # Adiciona a camada Stadia.AlidadeSatellite
         folium.TileLayer(
-            tiles='https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.png',
+            tiles='https://{s}.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.jpg',
             attr='&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
             name='Alidade Satellite'
         ).add_to(mapa)
