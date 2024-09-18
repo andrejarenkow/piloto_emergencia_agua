38935635a0b5b85f6e3f6ed173012dbba57694c5
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-09-18 13:52:56 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -226,7 +226,7 @@ Essas faixas ajudam a interpretar a qualidade da água com base nos limites esta
         # Adicionar um retângulo para pintar o fundo
         fig.add_shape(
             type="rect",
-            x0=-0.5, x1=len(dados_resultados['Ensaio'].unique()) - 0.5,  # Abranger todas as categorias de 'Ensaio'
+            x0=-0.5, x1=len(dados_resultados_tratada['Ensaio'].unique()) - 0.5,  # Abranger todas as categorias de 'Ensaio'
             y0=0, y1=0.5,  # Definir os limites verticais do retângulo
             fillcolor="forestgreen",
             opacity=0.3,
@@ -236,7 +236,7 @@ Essas faixas ajudam a interpretar a qualidade da água com base nos limites esta
         
         fig.add_shape(
             type="rect",
-            x0=-0.5, x1=len(dados_resultados['Ensaio'].unique()) - 0.5,  # Abranger todas as categorias de 'Ensaio'
+            x0=-0.5, x1=len(dados_resultados_tratada['Ensaio'].unique()) - 0.5,  # Abranger todas as categorias de 'Ensaio'
             y0=0.5, y1=0.75,  # Definir os limites verticais do retângulo
             fillcolor="Gold",
             opacity=0.3,
@@ -245,7 +245,7 @@ Essas faixas ajudam a interpretar a qualidade da água com base nos limites esta
         )
         fig.add_shape(
             type="rect",
-            x0=-0.5, x1=len(dados_resultados['Ensaio'].unique()) - 0.5,  # Abranger todas as categorias de 'Ensaio'
+            x0=-0.5, x1=len(dados_resultados_tratada['Ensaio'].unique()) - 0.5,  # Abranger todas as categorias de 'Ensaio'
             y0=0.75, y1=1,  # Definir os limites verticais do retângulo
             fillcolor="Orange",
             opacity=0.3,
@@ -254,8 +254,8 @@ Essas faixas ajudam a interpretar a qualidade da água com base nos limites esta
         )
         fig.add_shape(
             type="rect",
-            x0=-0.5, x1=len(dados_resultados['Ensaio'].unique()) - 0.5,  # Abranger todas as categorias de 'Ensaio'
-            y0=1, y1=dados_resultados['Indicador'].max()*1.2,  # Definir os limites verticais do retângulo
+            x0=-0.5, x1=len(dados_resultados_tratada['Ensaio'].unique()) - 0.5,  # Abranger todas as categorias de 'Ensaio'
+            y0=1, y1=dados_resultados_tratada['Indicador'].max()*1.2,  # Definir os limites verticais do retângulo
             fillcolor="Red",
             opacity=0.3,
             layer="below",  # Colocar o retângulo atrás dos dados
